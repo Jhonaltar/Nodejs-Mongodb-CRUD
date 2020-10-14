f6b8a88cb387607c66fdddb033763b2cd1ee78d9
Jhonaltar <jhon96alta@gmail.com>
2020-10-14 03:17:31 +0000

    Nodejs & Mongodb V1.1

--- src/database.js
+++ src/database.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+
 /* const {NOTES_APP_MONGODB_HOST , NOTES_APP_MONGODB_DATABASE}= process.env;
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${ NOTES_APP_MONGODB_DATABASE}`; */
 
--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+
 
 //initializations
 const app = express();
 
+
 //settings
 app.set('port', process.env.PROT || 4000);
 app.set('views', path.join( __dirname + 'views'));
+app.engine('.hbs', exphbs({
+    
+}))
+
+
 //Middlewares
 app.use(express.urlencoded({extended: false}));
+
+
 //global varaibles
+
 
 //routes
 app.get('/',(req , res )=>{
     res.send('hello word');
 })
+
+
 //static files
 app.use(express.static(path.join( __dirname + 'public')));
 
